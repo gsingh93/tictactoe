7850927e6b97944e54728658589e7f1d30cf646d
Gulshan Singh <gulshan@umich.edu>
2013-03-16 00:18:34 -0400

    Added code to check if a player wins and display a message

--- src/com/learnjava/tictactoe/MainActivity.java
+++ src/com/learnjava/tictactoe/MainActivity.java
@@ -1,6 +1,7 @@
 package com.learnjava.tictactoe;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -8,7 +9,9 @@ import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
-	private int turn = 0;
+	private boolean playerOneTurn = true;
+
+	private int board[][] = new int[3][3];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,115 @@ public class MainActivity extends Activity {
 	public void buttonClicked(View v) {
 		ImageButton button = (ImageButton) v;
 
-		if (turn % 2 == 0) {
+		int num;
+		if (playerOneTurn == true) {
 			button.setImageResource(R.drawable.x);
-			turn++;
+			num = 1;
 		} else {
 			button.setImageResource(R.drawable.o);
-			turn++;
+			num = 2;
 		}
+
+		if (v.getId() == R.id.button00) {
+			board[0][0] = num;
+		} else if (v.getId() == R.id.button01) {
+			board[0][1] = num;
+		} else if (v.getId() == R.id.button02) {
+			board[0][2] = num;
+		} else if (v.getId() == R.id.button10) {
+			board[1][0] = num;
+		} else if (v.getId() == R.id.button11) {
+			board[1][1] = num;
+		} else if (v.getId() == R.id.button12) {
+			board[1][2] = num;
+		} else if (v.getId() == R.id.button20) {
+			board[2][0] = num;
+		} else if (v.getId() == R.id.button21) {
+			board[2][1] = num;
+		} else if (v.getId() == R.id.button22) {
+			board[2][2] = num;
+		}
+
+		// See if anyone won
+		checkGame();
+
+		playerOneTurn = !playerOneTurn;
+	}
+
+	private void checkGame() {
+		// First row
+		if (board[0][0] == board[0][1] && board[0][1] == board[0][2]) {
+			if (board[0][0] != 0) {
+				displayWinDialog();
+				return;
+			}
+		}
+
+		// Second row
+		if (board[1][0] == board[1][1] && board[1][1] == board[1][2]) {
+			if (board[1][0] != 0) {
+				displayWinDialog();
+				return;
+			}
+		}
+
+		// Third row
+		if (board[2][0] == board[2][1] && board[2][1] == board[2][2]) {
+			if (board[2][0] != 0) {
+				displayWinDialog();
+				return;
+			}
+		}
+
+		// First column
+		if (board[0][0] == board[1][0] && board[1][0] == board[2][0]) {
+			if (board[0][0] != 0) {
+				displayWinDialog();
+				return;
+			}
+		}
+
+		// Second column
+		if (board[0][1] == board[1][1] && board[1][1] == board[2][1]) {
+			if (board[0][1] != 0) {
+				displayWinDialog();
+				return;
+			}
+		}
+
+		// Third column
+		if (board[0][2] == board[1][2] && board[1][2] == board[2][2]) {
+			if (board[0][2] != 0) {
+				displayWinDialog();
+				return;
+			}
+		}
+
+		// Top left to bottom right diagonal
+		if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+			if (board[0][0] != 0) {
+				displayWinDialog();
+				return;
+			}
+		}
+
+		// Top right to bottom left diagonal
+		if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+			if (board[0][2] != 0) {
+				displayWinDialog();
+				return;
+			}
+		}
+	}
+
+	private void displayWinDialog() {
+		String message;
+		if (playerOneTurn == true) {
+			message = "Player one wins!";
+		} else {
+			message = "Player two wins!";
+		}
+
+		new AlertDialog.Builder(this).setMessage(message).show();
 	}
 }
